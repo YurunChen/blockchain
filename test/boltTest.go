2b3a3254ed346b686b4465fff5a06fb76cb0c56a
YurunChen <1657503372@qq.com>
2022-11-12 10:23:34 +0800

    bolt数据库读操作实现

--- test/boltTest.go
+++ test/boltTest.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/boltdb/bolt"
 	"log"
 )
 
 func main() {
 	db, err := bolt.Open("test.db", 0600, nil)
+	defer db.Close()
 	if err != nil {
 		log.Panic("打开数据库失败！")
 	}
@@ -20,6 +22,17 @@ func main() {
 		}
 		bucket.Put([]byte("1111"), []byte("hello"))
 		bucket.Put([]byte("2222"), []byte("world"))
+		return nil
+	})
+	db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("b1"))
+		if bucket == nil {
+			log.Panic("bucket不存在，请检查！！")
+		}
+		v1 := bucket.Get([]byte("1111"))
+		v2 := bucket.Get([]byte("2222"))
+		fmt.Printf("数据1：%s\n", v1)
+		fmt.Printf("数据2：%s\n", v2)
 		return nil
 	})
 
